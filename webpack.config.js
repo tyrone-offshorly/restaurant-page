0f1659780e1fc83e9ca75c1d0f37e245ab245b17
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-12-04 11:49:44 +0800

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+}
